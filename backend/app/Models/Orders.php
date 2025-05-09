e07da198b6d6b0a5020f820c7981668d43c11edf
baska <baasankhuu928@gmail.com>
2025-05-09 20:53:57 +0800

    n

--- backend/app/Models/Orders.php
+++ backend/app/Models/Orders.php
@@ -8,7 +8,7 @@ use Illuminate\Database\Eloquent\Model;
 class Orders extends Model
 {
     use HasFactory;
-    protected $fillable = [
+    protected $fillable =[
         'prize',
         'count',
         'type',
